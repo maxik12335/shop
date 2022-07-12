d5bbf1af1d93418c3d41c2a42f6cee207556e87b
Max <maxim.it@bk.ru>
2022-07-12 14:04:50 +0300

    beta + alfa 2

--- script.js
+++ script.js
@@ -3,8 +3,47 @@ const postTitle = document.querySelectorAll('.post__title'),
     postBody = document.querySelectorAll('.post__body')
 
 postTitle.forEach((post, i) => {
-    postTitle[i].addEventListener(() => {
+    post.addEventListener('click', () => {
+        postTitleBorder.forEach(border => {
+            border.style.width = '0'
+        })
         postTitleBorder[i].style.width = '100%'
-        postBody[i].style.opacity = '1'
+
+        postBody.forEach(body => {
+            body.style.opacity = 0
+        })
+
+        postBody[i].style.opacity = 1
     })
+})
+
+
+
+const productesListItem = document.querySelectorAll('.productes-list__item')
+productesListItem[0].addEventListener('click', () => {
+    productesListItem.forEach(item => {
+        item.style.color = 'black'
+    })
+    productesListItem[0].style.color = '#fffb9a';
+})
+
+productesListItem[2].addEventListener('click', () => {
+    productesListItem.forEach(item => {
+        item.style.color = 'black'
+    })
+    productesListItem[2].style.color = '#fffb9a';
+})
+
+productesListItem[1].addEventListener('click', () => {
+    productesListItem.forEach(item => {
+        item.style.color = 'black'
+    })
+    productesListItem[1].style.color = 'silver';
+})
+
+productesListItem[3].addEventListener('click', () => {
+    productesListItem.forEach(item => {
+        item.style.color = 'black'
+    })
+    productesListItem[3].style.color = 'silver';
 })
